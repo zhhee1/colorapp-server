0b6a22ffef500c613ac08a5cf521382fae53d932
zhhee1 <zhhee1@student.monash.edu>
2016-12-28 21:50:05 +0800

    server

---
+++ index.js
@@ -0,0 +1,40 @@
+// assign variables
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var _ = require('lodash');
+
+// Create the application
+var app = express();
+
+// Add Middlewares necessary for REST API's (essentially interrupt or intercept request)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// CORS Support
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
+// Connect to MongoDB
+// mongoose.connect('mongodb://localhost/colorapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/colorapp');
+mongoose.connection.once('open', function () {
+
+    //Load the models
+    app.models = require('./models/index');
+
+    // Load the routes
+    var routes = require('./routes');
+    _.each(routes, function (controller, route) {
+        app.use(route, controller(app, route));
+    });
+
+    console.log('servers up and running');
+    app.listen(3000);
+})
